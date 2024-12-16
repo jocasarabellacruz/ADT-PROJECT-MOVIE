c7f8998ccf294f871af7f1f0108b9a93b2a2305d
Zyldzkie <jocasarabella@gmail.com>
2024-12-16 17:05:42 +0800

    limit CORS

--- movieproject-api/restapi/admin_search.php
+++ movieproject-api/restapi/admin_search.php
@@ -2,11 +2,10 @@
 session_start();
 
 header('Content-Type: application/json');
-header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Origin: http://localhost:3000");
 header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization, Cookie");
 header("Access-Control-Allow-Credentials: true");
-
 require("codes/connection.php");
 require 'vendor/autoload.php';
 
