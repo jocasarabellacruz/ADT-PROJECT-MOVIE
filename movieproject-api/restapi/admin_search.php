2960de999c70f20aa89d751a7fa5f8ad434939fa
Zyldzkie <jocasarabella@gmail.com>
2024-12-18 16:31:12 +0800

    Update admin_search.php

--- movieproject-api/restapi/admin_search.php
+++ movieproject-api/restapi/admin_search.php
@@ -9,34 +9,41 @@ header("Access-Control-Allow-Credentials: true");
 require("codes/connection.php");
 require 'vendor/autoload.php';
 
+
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
 $dotenv->load();
 
 use GuzzleHttp\Client;
 
-function searchMovies($query) {
-    $apiKey = $_ENV['TMDB_API_KEY']; 
-    $client = new Client();
-    
-    // Construct the search URL
-    $url = "https://api.themoviedb.org/3/search/movie?api_key={$apiKey}&query=" . urlencode($query);
-    
-    // Make the request
-    $response = $client->request('GET', $url, [
-        'headers' => [
-            'accept' => 'application/json',
-        ],
-    ]);
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
 
-    return json_decode($response->getBody(), true);
-}
+    function searchMovies($query) {
+        $apiKey = $_ENV['TMDB_API_KEY']; 
+        $client = new Client();
+        
+        // Construct the search URL
+        $url = "https://api.themoviedb.org/3/search/movie?api_key={$apiKey}&query=" . urlencode($query);
+        
+        // Make the request
+        $response = $client->request('GET', $url, [
+            'headers' => [
+                'accept' => 'application/json',
+            ],
+        ]);
 
-// Check if a search query is provided
-if (isset($_GET['query'])) {
-    $query = $_GET['query'];
-    $result = searchMovies($query);
-    echo json_encode($result);
+        return json_decode($response->getBody(), true);
+    }
+
+    // Check if a search query is provided
+    if (isset($_GET['query'])) {
+        $query = $_GET['query'];
+        $result = searchMovies($query);
+        echo json_encode($result);
+    } else {
+        echo json_encode(['status' => 'error', 'error' => 'No Search Query Provided.']);
+    }
+
 } else {
-    echo json_encode(['error' => 'No search query provided']);
+    echo json_encode(['status' => 'error', 'error' => 'Wrong Request Method']);
 }
 ?>
