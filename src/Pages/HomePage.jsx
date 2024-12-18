961cbcdf19598447bc5555709f73e4a7c416645e
Zyldzkie <jocasarabella@gmail.com>
2024-12-18 16:40:28 +0800

    Update HomePage design

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,10 +1,14 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import NavBar from '../Components/NavBar';
 import FeaturedMovies from '../Components/FeaturedMovies';
 import MainMoviesPanel from '../Components/MainMoviesPanel';
 import SearchBar from '../Components/SearchBar';
 import FavoritesPanel from '../Components/FavoritesPanel';
+import Account from '../Components/Account';
+import axios from 'axios';
 import './HomePage.css';
+
+axios.defaults.withCredentials = true;
 
 const HomePage = () => {
   const [movies, setMovies] = useState([
@@ -26,10 +30,25 @@ const HomePage = () => {
       poster: 'https://via.placeholder.com/150',
       description: 'A gritty and realistic take on Batman.',
     },
+    
   ]);
 
   const [favorites, setFavorites] = useState([]);
   const [searchTerm, setSearchTerm] = useState('');
+
+  const getUser = async () => {
+    try {
+      const response = await axios.get("http://localhost/get_user");
+      console.log('User:', response.data);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+  
+
+  useEffect(() => {
+    getUser();
+  }, []); 
 
   // Handle search input
   const handleSearch = (term) => {
@@ -63,13 +82,17 @@ const HomePage = () => {
       </div>
       <div className="main-content">
         <div className="top-row">
-          <div className="featured">
-            <FeaturedMovies featured={filteredMovies.slice(0, 5)} />
+          <div className="account">
+            <Account />
           </div>
           <div className="search">
             <SearchBar onSearch={handleSearch} />
           </div>
+          
         </div>
+        <div className="featured">
+            <FeaturedMovies featured={filteredMovies.slice(0, 5)} />
+          </div>
         <div className="bottom-row">
           <div className="main-movies">
             <MainMoviesPanel
