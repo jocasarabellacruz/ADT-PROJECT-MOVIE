d48c8635614e471400437e96f9f9967269f48471
Zyldzkie <jocasarabella@gmail.com>
2024-12-18 16:42:01 +0800

    adminsearchmovie design

--- src/Components/AdminSearchMovie.jsx
+++ src/Components/AdminSearchMovie.jsx
@@ -3,6 +3,8 @@ import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import './AdminSearchMovie.css';
 
+axios.defaults.withCredentials = true;
+
 const AdminSearchMovie = () => {
   const [query, setQuery] = useState('');
   const [results, setResults] = useState([]);
@@ -11,6 +13,7 @@ const AdminSearchMovie = () => {
 
   const handleSearch = async () => {
     try {
+    
       const response = await axios.get(`http://localhost/admin_search?query=${query}`);
       
       console.log(response.data.results[0].original_title); 
@@ -32,17 +35,20 @@ const AdminSearchMovie = () => {
   };
 
   return (
-    <div>
-      <h1>Admin Search Movie</h1>
-      <input
-        type="text"
-        value={query}
-        onChange={(e) => setQuery(e.target.value)}
-        placeholder="Search for a movie..."
-      />
-      <button onClick={handleSearch}>Search</button>
+    <div className="container">
+      <div className="search-card">
+        <h1>Admin Search Movie</h1>
+        <input
+        className='input'
+          type="text"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+          placeholder="Search for a movie..."
+        />
+        <button className="search-button" onClick={handleSearch}>Search</button>
 
-      {error && <p>{error}</p>}
+        {error && <p>{error}</p>}
+      </div>
 
       <div className="movie-list">
         {results.map((movie) => ( 
@@ -52,7 +58,7 @@ const AdminSearchMovie = () => {
             onClick={() => handleCardClick(movie.id)}
           >
             <h2>{movie.original_title}</h2>
-            <img className="movie-poster" src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.original_title} />
+            <img className="movie-poster" src={`https://image.tmdb.org/t/p/original${movie.poster_path}`} alt={movie.original_title} />
             <p>{movie.overview}</p>
           </div>
         ))}
